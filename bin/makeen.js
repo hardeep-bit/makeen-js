e684a7bfbf44cb2bc65752d2cff7aad36573fe94
hardeep-bit <hardeepsingh12994@gmail.com>
2025-06-07 02:04:12 +0530

    file fixes

--- bin/makeen.js
+++ bin/makeen.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const cdk = require('aws-cdk-lib');
-const { TextProcessingStack } = require('../lib/text-processing-poc-stack');
+const MakeenStack = require('../lib/makeenStack');
 require('dotenv').config();
 
 const app = new cdk.App();
-new TextProcessingStack(app, 'TextProcessingStack');
+new MakeenStack(app, 'MakeenStack');
